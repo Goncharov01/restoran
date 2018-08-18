0c8909354d58e4313b1011bab0b9898739b761ac
Goncharov01 <mr.sacha.goncharov.01@mail.ru>
2018-08-18 17:43:54 +0300

    user

--- src/main/kotlin/menu/restoran/Restaurant.kt
+++ src/main/kotlin/menu/restoran/Restaurant.kt
@@ -22,8 +22,9 @@ class Restaurant {
 
     @OneToMany(mappedBy = "restaurant")
     var menus: List<Menu>? = null
-
-    override fun toString() = "Restaurant($id, $name, $location, $lat, $lng, ${menus?.map { it.id }})"
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    var user: User? = null
 
     override fun equals(other: Any?): Boolean {
         if (this === other) return true
@@ -37,6 +38,7 @@ class Restaurant {
         if (lat != other.lat) return false
         if (lng != other.lng) return false
         if (menus != other.menus) return false
+        if (user != other.user) return false
 
         return true
     }
@@ -47,7 +49,40 @@ class Restaurant {
         result = 31 * result + (location?.hashCode() ?: 0)
         result = 31 * result + (lat?.hashCode() ?: 0)
         result = 31 * result + (lng?.hashCode() ?: 0)
+        result = 31 * result + (menus?.hashCode() ?: 0)
+        result = 31 * result + (user?.hashCode() ?: 0)
         return result
     }
+
+    override fun toString(): String {
+        return "Restaurant(id=$id, name=$name, location=$location, lat=$lat, lng=$lng, menus=$menus, user=$user)"
+    }
+
+//    override fun toString() = "Restaurant($id, $name, $location, $lat, $lng, ${menus?.map { it.id }})"
+//
+//    override fun equals(other: Any?): Boolean {
+//        if (this === other) return true
+//        if (javaClass != other?.javaClass) return false
+//
+//        other as Restaurant
+//
+//        if (id != other.id) return false
+//        if (name != other.name) return false
+//        if (location != other.location) return false
+//        if (lat != other.lat) return false
+//        if (lng != other.lng) return false
+//        if (menus != other.menus) return false
+//
+//        return true
+//    }
+//
+//    override fun hashCode(): Int {
+//        var result = id?.hashCode() ?: 0
+//        result = 31 * result + (name?.hashCode() ?: 0)
+//        result = 31 * result + (location?.hashCode() ?: 0)
+//        result = 31 * result + (lat?.hashCode() ?: 0)
+//        result = 31 * result + (lng?.hashCode() ?: 0)
+//        return result
+//    }
 
 }
---
+++ src/main/kotlin/menu/restoran/User.kt
@@ -0,0 +1,71 @@
+package menu.restoran
+
+import javax.persistence.*
+
+@Entity
+@Table(name = "app_user")
+class User(
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @Column(name = "id")
+  var id: Long? = -1,
+
+  @Column(name = "username")
+  var username: String? = null,
+
+  @Column(name = "password")
+  // @JsonIgnore
+  var password: String? = null,
+
+  @Column(name = "first_name")
+  var firstName: String? = null,
+
+  @Column(name = "last_name")
+  var lastName: String? = null,
+
+//  @ManyToMany(fetch = FetchType.EAGER)
+//  @JoinTable(
+//    name = "user_role",
+//    joinColumns = [JoinColumn(name = "user_id", referencedColumnName = "id")],
+//    inverseJoinColumns = [JoinColumn(name = "role_id", referencedColumnName = "id")]
+//  )
+//  var roles: Collection<Role>? = null,
+  @OneToMany(mappedBy = "user")
+  var restaurants:List<Restaurant>? = null
+
+//@JoinColumn (name="company")
+
+
+
+) {
+  override fun equals(other: Any?): Boolean {
+    if (this === other) return true
+    if (javaClass != other?.javaClass) return false
+
+    other as User
+
+    if (id != other.id) return false
+    if (username != other.username) return false
+    if (password != other.password) return false
+    if (firstName != other.firstName) return false
+    if (lastName != other.lastName) return false
+    if (restaurants != other.restaurants) return false
+
+    return true
+  }
+
+  override fun hashCode(): Int {
+    var result = id?.hashCode() ?: 0
+    result = 31 * result + (username?.hashCode() ?: 0)
+    result = 31 * result + (password?.hashCode() ?: 0)
+    result = 31 * result + (firstName?.hashCode() ?: 0)
+    result = 31 * result + (lastName?.hashCode() ?: 0)
+    result = 31 * result + (restaurants?.hashCode() ?: 0)
+    return result
+  }
+
+  override fun toString(): String {
+    return "User(id=$id, username=$username, password=$password, firstName=$firstName, lastName=$lastName, restaurants=$restaurants)"
+  }
+}
+
